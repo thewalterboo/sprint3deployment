78499f4702902b7b0d6390467d3c0024c19d0c67
Walter Boo Keng Hua <bookenghua@outlook.com>
2023-05-10 20:25:56 +0800

    syntax

--- pages.py
+++ pages.py
@@ -140,7 +140,7 @@ class Pages:
             "These are the recommended songs from our engine."
         )
         
-        components.html("<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2GrafQPopdSdPobRxU6OST?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>")
+        components.html('<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2GrafQPopdSdPobRxU6OST?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>')
 
         
 
